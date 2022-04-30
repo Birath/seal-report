f4542dffefe0c3c6dbb4c3f3d4a1f0186044a9f2
Björn Birath <bjorn@birath.dev>
2022-04-30 22:33:42 +0200

    add preload screen for getting manifest

--- src/Constants.cs
+++ src/Constants.cs
@@ -2,11 +2,17 @@
 {
     public static class Constants
     {
-        public const string DefinitionDataBaseName = "DestinyDefinitions";
+        public const string BungieApiKey = "deb11608535f443c9f85e72beac40252";
 
+        public const string DefinitionDataBaseName = "DestinyDefinitions";
+       
         public const string RecordStoreName = "Records";
         public const string LoreStoreName = "Lore";
         public const string ObjectivesStoreName = "Objectives";
         public const string PresentationNodesStoreName = "PresentationNodes";
+
+        public const string BungieApiEndpoint = "https://www.bungie.net/Platform";
+        public const string BungieManifestEndpoint = "https://www.bungie.net";
+
     }
 }
